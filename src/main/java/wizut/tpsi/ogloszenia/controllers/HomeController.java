c3166dc042b45c09337a0bd2961c6c4e1fc354b4
Alicja <a.berlinska@gmail.com>
2020-02-15 11:43:01 +0100

    Done task one from lab 11

--- src/main/java/wizut/tpsi/ogloszenia/controllers/HomeController.java
+++ src/main/java/wizut/tpsi/ogloszenia/controllers/HomeController.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import wizut.tpsi.ogloszenia.jpa.BodyStyle;
 import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
+import wizut.tpsi.ogloszenia.jpa.CarModel;
+import wizut.tpsi.ogloszenia.jpa.FuelType;
 import wizut.tpsi.ogloszenia.services.OffersService;
 
 @Controller
@@ -24,8 +27,18 @@ public class HomeController {
         List<CarManufacturer> carManufacturers = os.getCarManufacturers();
         model.addAttribute("carManufacturers", carManufacturers);
     
+        List<BodyStyle> bodyStyle = os.getBodyStyles();
+        model.addAttribute("bodyStyle", bodyStyle);
+        
+        List<FuelType> fuelType = os.getFuelTypes();
+        model.addAttribute("fuelType", fuelType);
+        
+        List<CarModel> carModel = os.getCarModels(3);
+        model.addAttribute("carModel", carModel);     
+        
         model.addAttribute("carModel2", os.getModel(2).getManufacturer().getName());
         model.addAttribute("getManufacturer2", os.getModel(2).getName());
+        
         return "offersList";
     }
 }
