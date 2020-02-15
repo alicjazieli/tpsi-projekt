a2d93f9712e12f7b72808a6f77fc05c8e9c609dc
Alicja <a.berlinska@gmail.com>
2020-02-15 15:59:15 +0100

    Done task two from lab 11

--- src/main/java/wizut/tpsi/ogloszenia/controllers/HomeController.java
+++ src/main/java/wizut/tpsi/ogloszenia/controllers/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import wizut.tpsi.ogloszenia.jpa.BodyStyle;
 import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
 import wizut.tpsi.ogloszenia.jpa.CarModel;
@@ -26,32 +27,37 @@ public class HomeController {
     public String home(Model model) {
         
         List<CarManufacturer> carManufacturers = os.getCarManufacturers();
+        List<CarModel> carModels = os.getCarModels(3);
+
+        List<Offer> offers = os.getOffers();
+
         model.addAttribute("carManufacturers", carManufacturers);
-    
-        List<BodyStyle> bodyStyle = os.getBodyStyles();
-        model.addAttribute("bodyStyle", bodyStyle);
-        
-        List<FuelType> fuelType = os.getFuelTypes();
-        model.addAttribute("fuelType", fuelType);
-        
-        List<CarModel> carModel = os.getCarModels(3);
-        model.addAttribute("carModel", carModel);    
-        
-        List<Offer> carOffer = os.getOffersByModel(10);
-        model.addAttribute("carOffer", carOffer);
-        
-//        List<Offer> carOfferMan = os.getOffersByManufacturer(2);
-//        model.addAttribute("carOfferMan", carOfferMan);
-        
-        List<Offer> offerTitle = os.getOffers();
-        model.addAttribute("offerTitle", offerTitle);
-        
-        
-        model.addAttribute("carModel2", os.getModel(2).getManufacturer().getName());
-        model.addAttribute("getManufacturer2", os.getModel(2).getName());
-        
-        model.addAttribute("carsOffer", os.getOffer(2));
-        
+        model.addAttribute("carModels", carModels);
+        model.addAttribute("offers", offers);
+
         return "offersList";
+       
+    
+//        List<BodyStyle> bodyStyle = os.getBodyStyles();
+//        model.addAttribute("bodyStyle", bodyStyle);
+//        
+//        List<FuelType> fuelType = os.getFuelTypes();
+//        model.addAttribute("fuelType", fuelType);
+//
+//        List<Offer> carOffer = os.getOffersByModel(10);
+//        model.addAttribute("carOffer", carOffer);
+//        
+//        List<Offer> carOfferMan = os.getOffersByManufacturer(2);
+//        model.addAttribute("carOfferMan", carOfferMan);           
+//        
+//        model.addAttribute("carModel2", os.getModel(2).getManufacturer().getName());
+//        model.addAttribute("getManufacturer2", os.getModel(2).getName());
+
     }
+    @GetMapping("/offer/{id}")
+        public String offerDetails(Model model, @PathVariable("id") Integer id) {
+            Offer offer = os.getOffer(id);
+            model.addAttribute("offer", offer);
+            return "offerDetails";
+        }
 }
