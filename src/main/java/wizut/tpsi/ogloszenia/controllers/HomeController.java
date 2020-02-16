02c84014761adc66897d4314f4aff67e3f420623
Alicja <a.berlinska@gmail.com>
2020-02-16 20:33:29 +0100

    Adding deleting and editing offer function

--- src/main/java/wizut/tpsi/ogloszenia/controllers/HomeController.java
+++ src/main/java/wizut/tpsi/ogloszenia/controllers/HomeController.java
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import wizut.tpsi.ogloszenia.jpa.BodyStyle;
 import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
 import wizut.tpsi.ogloszenia.jpa.CarModel;
@@ -63,6 +65,7 @@ public class HomeController {
         
     return "offersList";
     }
+    
     @GetMapping("/offer/{id}")
         public String offerDetails(Model model, @PathVariable("id") Integer id) {
             Offer offer = os.getOffer(id);
@@ -81,10 +84,78 @@ public class HomeController {
                 model.addAttribute("bodyStyles", bodyStyles);
                 model.addAttribute("fuelTypes", fuelTypes);
                 
+                model.addAttribute("header", "Nowe ogłoszenie");
+                model.addAttribute("action", "/newoffer");
+                
             return "offerForm";
         }
         offer = os.createOffer(offer);
 
     return "redirect:/offer/" + offer.getId();
+    }
+    
+    @PostMapping("/newoffer")
+        public String saveNewOffer(Model model, @Valid Offer offer, BindingResult binding) {
+            if(binding.hasErrors()) {
+                List<CarModel> carModels = os.getCarModels();
+                List<BodyStyle> bodyStyles = os.getBodyStyles();
+                List<FuelType> fuelTypes = os.getFuelTypes();
+                model.addAttribute("carModels", carModels);
+                model.addAttribute("bodyStyles", bodyStyles);
+                model.addAttribute("fuelTypes", fuelTypes);
+                
+                model.addAttribute("header", "Nowe ogłoszenie");
+                model.addAttribute("action", "/newoffer");
+            return "offerForm";
+        }
+        offer = os.createOffer(offer);
+        return "redirect:/offer/" + offer.getId();
+        }
+        
+    @GetMapping("/deleteoffer/{id}")
+        public String deleteOffer(Model model, @PathVariable("id") Integer id) {
+            Offer offer = os.deleteOffer(id);
+
+            model.addAttribute("offer", offer);
+            return "deleteOffer";
+        }
+        
+    @GetMapping("/editoffer/{id}")
+        public String editOffer(Model model, @PathVariable("id") Integer id) {
+            Offer offer = os.getOffer(id);
+            model.addAttribute("offer", offer);
+            List<CarModel> carModels = os.getCarModels();
+            List<BodyStyle> bodyStyles = os.getBodyStyles();
+            List<FuelType> fuelTypes = os.getFuelTypes();
+
+            model.addAttribute("carModels", carModels);
+            model.addAttribute("bodyStyles", bodyStyles);
+            model.addAttribute("fuelTypes", fuelTypes);
+        
+            return "offerForm";
+        }
+    @PostMapping("/editoffer/{id}")
+        public String saveEditedOffer(Model model, @PathVariable("id") Integer id, @Valid Offer offer, BindingResult binding) {
+            if(binding.hasErrors()) {
+                model.addAttribute("header", "Edycja ogłoszenia");
+                model.addAttribute("action", "/editoffer/" + id);
+
+                List<CarModel> carModels = os.getCarModels();
+                List<BodyStyle> bodyStyles = os.getBodyStyles();
+                List<FuelType> fuelTypes = os.getFuelTypes();
+
+                model.addAttribute("carModels", carModels);
+                model.addAttribute("bodyStyles", bodyStyles);
+                model.addAttribute("fuelTypes", fuelTypes);
+
+                return "offerForm";
+            }
+
+            os.saveOffer(offer);
+
+            return "redirect:/offer/" + offer.getId();
+    }       
 }
-}
+        
+    
+
